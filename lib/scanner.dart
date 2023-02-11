bdcf4afe10125b3963aa201d668622d182fb127f
Joe Monkila <jnkiwa25@gmail.com>
2023-02-11 10:09:55 +0100

    commit

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,4 +1,7 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:simple_permissions/simple_permissions.dart';
 
 class Scanner extends StatefulWidget{
   @override
@@ -9,10 +12,63 @@ class Scanner extends StatefulWidget{
   
 }
 class ScannerState extends State<Scanner>{
+  String _reader='';
+  Permission permission= Permission.Camera;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.pinkAccent,
+      home:new Scaffold(
+        appBar: new AppBar(title: new Text("Qr Scanner by Monkila"),backgroundColor: Colors.orange,),
+        body: new Column(
+          children: <Widget>[
+
+            new Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+            ),
+            new RaisedButton(
+
+              splashColor: Colors.pinkAccent,
+              color: Colors.red,
+              child: new Text("Scan",style: new TextStyle(fontSize: 20.0,color: Colors.white),),
+              onPressed: scan,
+            ),
+            new Padding(padding: const EdgeInsets.symmetric(vertical: 10.0), ),
+            new Text('$_reader',softWrap: true, style: new TextStyle(fontSize: 30.0,color: Colors.blue),),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  requestPermission() async {
+    bool result = (await SimplePermissions.requestPermission(permission)) as bool;
+    setState(()=> new SnackBar
+      (backgroundColor: Colors.red,content: new Text(" $result"),),
+
+    );
+  }
+  scan() async {
+    try {
+      String reader= await BarcodeScanner.scan();
+
+      if (!mounted) {
+        return;
+      }
+
+      setState(() => this._reader=reader);
+    } on PlatformException catch(e) {
+      if(e.code==BarcodeScanner.CameraAccessDenied) {requestPermission();}
+      else{setState(()=> _reader = "unknown exception $e");}
+    }on FormatException{
+      setState(() => _reader = 'null (User returned using the "back"-button before scanning anything. Result)');
+    } catch (e) {
+      setState(() => _reader = 'Unknown error: $e');
+    }
+
   }
   
 }
